1ca30fa681f4a2bb76179b4f88521c56906fdbf7
julia <65985506+campanula@users.noreply.github.com>
2022-07-16 20:25:10 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,50 +1,25 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
 
+ninetyDaysInSeconds = 90*24*60*60;
 
+app.use(helmet({
+  frameguard: {         // configure
+    action: 'deny'
+  },
+  contentSecurityPolicy: {    // enable and configure
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ['style.com'],
+      scriptSrc: ["'self'", "trusted-cdn.com"],
+    }
+  },
+  dnsPrefetchControl: false     // disable
+}))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use(helmet.noCache())
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}))
 
 
 module.exports = app;
